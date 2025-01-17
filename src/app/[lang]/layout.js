b2af09c54df8960a218ff2d3946e3d4bbb5fc800
yiGmMk <marvelousme@163.com>
2025-01-17 10:31:03 +0000

    config: analysis

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -51,7 +51,7 @@ export default async function Layout({ children, params: { lang, slug = [] } }) 
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "hvful8k59h");
+              })(window, document, "clarity", "script", "pv9syjwm7h");
             `,
           }}
         />
